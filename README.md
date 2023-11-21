2f2589523e146bc6d27c2400ac7372600d710d5a
Yazawa Manabu <manabu.yazawa@plaid.co.jp>
2023-11-21 15:28:06 +0900

    feat: first commit

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # create_backend_bucket_with_cdn_settings
+
 googleapis を使ってBackend Bucketを作成し、Cloud CDNを設定するサンプル
+
+# Usage
+
+事前にCloud Storageのバケットを作成しておく。
+
+```
+npx ts-node src/index.ts --bucket=<YOUR_BUCKET_NAME> --project=<YOUR_GOOGLE_CLOUD_PROJECT_ID>
+```
---
+++ src/index.ts
@@ -0,0 +1,49 @@
+import { google } from "googleapis";
+import { program } from "commander";
+
+program
+  .option("--bucket <bucket>", "Bucket name")
+  .option("--project <project>", "GCP Project ID");
+program.parse();
+
+const googleAuthOpts = {
+  scopes: ["https://www.googleapis.com/auth/cloud-platform"],
+};
+const auth = new google.auth.GoogleAuth(googleAuthOpts);
+
+async function main() {
+  const options = program.opts();
+  console.log(options);
+  const bucketName = options.bucket;
+  const gcpProjectId = options.project;
+  if (!bucketName) {
+    throw new Error("--bucket option is required");
+  }
+
+  const computeClient = google.compute({ version: "v1", auth });
+
+  const res = await computeClient.backendBuckets.insert({
+    project: gcpProjectId,
+    requestBody: {
+      name: bucketName,
+      bucketName: bucketName,
+      enableCdn: true,
+      cdnPolicy: {
+        requestCoalescing: true,
+        cacheMode: "CACHE_ALL_STATIC",
+        defaultTtl: 3600,
+        maxTtl: 3600,
+        clientTtl: 0,
+        negativeCaching: false,
+        serveWhileStale: 0,
+      },
+      compressionMode: "DISABLED",
+    },
+  });
+
+  console.log(JSON.stringify(res.data, null, 2));
+}
+
+(async () => {
+  await main();
+})();
